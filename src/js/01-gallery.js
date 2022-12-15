c2ac16201ec5b5097441214bb96e79fe69e15cab
MichalCaputa <Taki_Sobie@Interia.pl>
2022-12-16 00:25:26 +0100

    update
    W koncu

---
+++ src/js/01-gallery.js
@@ -0,0 +1,8 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+console.log(galleryItems);
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
