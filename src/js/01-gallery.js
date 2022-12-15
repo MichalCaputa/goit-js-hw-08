37717be6061fe944216ceb383d6b369a2f1e18ad
MichalCaputa <Taki_Sobie@Interia.pl>
2022-12-16 00:45:32 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 console.log(galleryItems);
-// Opisany w dokumentacji
-import SimpleLightbox from 'simplelightbox';
-// Dodatkowy import stylów
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// // Opisany w dokumentacji
+// import SimpleLightbox from 'simplelightbox';
+// // Dodatkowy import stylów
+// import 'simplelightbox/dist/simple-lightbox.min.css';
